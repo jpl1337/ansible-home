9f392ea6ec7c8516697c1b0b1478b0e0850f11ff
Radio <jeremypena1337@gmail.com>
2023-08-30 18:11:20 -0400

    comment out telegram notifications, comment out ansible pull line in bootstrap, fix bootstrap formating

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 To set up new machine run bootstrap script:
 
 ```bash <(curl -sSL "https://raw.githubusercontent.com/jpl1337/ansible-home/master/bootstrap.sh")```
+or 
+```bash <(wget -q0- "https://raw.githubusercontent.com/jpl1337/ansible-home/master/bootstrap.sh")```
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 # This script will be used to bootstrap the ansible process
 # The idea is that a fresh install won't have git or ansible installed
 # Which are required to grab the playbook and run it
+printf "\nUpdating....\n\n"
 sudo apt update
+printf "\nInstalling ansible...\n\n"
 sudo apt install git ansible -y 
-ansible-pull -U https://github.com/jpl1337/ansible-home.git
+printf "\nRunning ansible-home playbook...\n\n"
+# ansible-pull -U https://github.com/jpl1337/ansible-home.git
